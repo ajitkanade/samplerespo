f0b42deab0f3086b24faad8e6a1401e5d1fe2d04
ajitkanade <ajitkanade23@gmail.com>
2025-11-25 13:46:59 +0530

    Update polyMorph.js

---
+++ practice/polyMorph.js
@@ -0,0 +1,22 @@
+
+class Animal{
+    speak(){
+        console.log("Animal speak in different language");
+            }
+}
+class Dog{
+    speak(){
+    console.log("Bhaw Bhaw");
+           }
+}
+class Cat{
+    speak(){
+        console.log("Mewo Mewdo");
+    }
+}
+
+const dog1=new Dog();
+dog1.speak();
+ 
+const cat1=new Cat();
+cat1.speak();
